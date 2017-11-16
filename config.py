aa5a3503f3164ec237eb74184a1d124731102497
Hobinx <binservmail@gmail.com>
2017-11-16 14:48:50 +0800

    fix typo

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEM_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///{}'.format(os.path.join(basedir, 'data-dev.sqlite'))
 
 

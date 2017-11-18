5f7f4cb104f9df67626d2d69c9aea3159453d4e3
Hobinx <binservmail@gmail.com>
2017-11-18 20:41:36 +0800

    add pagination for index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import abort, flash, render_template, redirect, url_for
+from flask import abort, current_app, flash, render_template, \
+    redirect, request, url_for
 from flask_login import current_user, login_required
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -16,8 +17,15 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
 
 
 @main.route('/user/<username>')

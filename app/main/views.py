81b3b45890f3a940bfd505cef0c098ae8e03aecf
Hobinx <binservmail@gmail.com>
2017-11-18 14:14:01 +0800

    edit profile route and template

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import flash, render_template, redirect, url_for
+from flask_login import current_user, login_required
 from . import main
+from .. import db
 from ..models import User
+from .forms import EditProfileForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -12,3 +15,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)

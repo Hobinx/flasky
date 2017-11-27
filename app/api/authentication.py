9655e05a922c4fbef129015c4d497e093c1e700e
Hobinx <binservmail@gmail.com>
2017-11-27 18:52:16 +0800

    add route for auth token generation

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,4 +1,4 @@
-from flask import g
+from flask import g, jsonify
 from flask_httpauth import HTTPBasicAuth
 from ..models import User
 from . import api
@@ -34,3 +34,11 @@ def before_request():
     if not g.current_user.is_anonymous and \
             not g.current_user.confirmed:
         return forbidden('Unconfirmed account')
+
+
+@api.route('/tokens/', methods=['POST'])
+def get_token():
+    if g.current_user.is_anonymous or g.token_used:
+        return unauthorized('Invalid credentials')
+    return jsonify({'token': g.current_user.generate_auth_token(
+        expiration=3600), 'expiration': 3600})
